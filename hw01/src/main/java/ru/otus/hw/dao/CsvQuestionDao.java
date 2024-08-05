8dadfbae7a8670ff6a591d80ed3c884a955d9d1f
pashentsev <pashentsev>
2024-08-06 01:18:47 +0300

    разбор csv

--- hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
+++ hw01/src/main/java/ru/otus/hw/dao/CsvQuestionDao.java
@@ -1,23 +1,62 @@
 package ru.otus.hw.dao;
 
+import com.opencsv.bean.CsvToBeanBuilder;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.dto.QuestionDto;
 import ru.otus.hw.domain.Question;
+import ru.otus.hw.exceptions.QuestionReadException;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
+@SuppressWarnings("rawtypes")
 @RequiredArgsConstructor
 public class CsvQuestionDao implements QuestionDao {
     private final TestFileNameProvider fileNameProvider;
 
     @Override
     public List<Question> findAll() {
-        // Использовать CsvToBean
-        // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
-        // Использовать QuestionReadException
-        // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+        FileReader fileReader = null;
 
-        return new ArrayList<>();
+            List<QuestionDto> beans;
+        beans = new CsvToBeanBuilder(readCvsFile(fileNameProvider.getTestFileName()))
+                .withType(QuestionDto.class).withSeparator(';').withSkipLines(1).withOrderedResults(false).build().parse();
+
+        // Использовать CsvToBean
+            // https://opencsv.sourceforge.net/#collection_based_bean_fields_one_to_many_mappings
+            // Использовать QuestionReadException
+            // Про ресурсы: https://mkyong.com/java/java-read-a-file-from-resources-folder/
+
+            return beans.stream().map(QuestionDto::toDomainObject).collect(Collectors.toList());
+
     }
+
+    private Reader readCvsFile(String path) throws QuestionReadException {
+        try {
+            return getFileFromResourceAsStream(path);
+        } catch (Exception e) {
+            throw new QuestionReadException(e.getMessage(), e);
+        }
+    }
+
+    private Reader getFileFromResourceAsStream(String fileName) {
+
+        // The class loader that loaded the class
+        ClassLoader classLoader = getClass().getClassLoader();
+        InputStream inputStream = classLoader.getResourceAsStream(fileName);
+
+        // the stream holding the file content
+        if (inputStream == null) {
+            throw new IllegalArgumentException("file not found! " + fileName);
+        } else {
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+          Reader reader = new BufferedReader(inputStreamReader);
+            return reader;
+        }
+
+    }
+
 }
