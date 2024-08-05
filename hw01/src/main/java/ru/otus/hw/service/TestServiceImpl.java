065ebdbd7dde85c43989c194b0a7afb90a26c062
pashentsev <pashentsev>
2024-08-05 23:00:04 +0300

    Добавил инициализацию бинов вызов Dao scv

--- hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
+++ hw01/src/main/java/ru/otus/hw/service/TestServiceImpl.java
@@ -1,16 +1,27 @@
 package ru.otus.hw.service;
 
 import lombok.RequiredArgsConstructor;
+import ru.otus.hw.config.TestFileNameProvider;
+import ru.otus.hw.dao.CsvQuestionDao;
+import ru.otus.hw.dao.QuestionDao;
 
 @RequiredArgsConstructor
 public class TestServiceImpl implements TestService {
 
     private final IOService ioService;
-
+    private final TestFileNameProvider fileNameProvider;
     @Override
     public void executeTest() {
         ioService.printLine("");
         ioService.printFormattedLine("Please answer the questions below%n");
         // Получить вопросы из дао и вывести их с вариантами ответов
+        QuestionDao questionDao = new CsvQuestionDao(fileNameProvider);
+        questionDao.findAll().forEach(
+                question -> {
+                    System.out.println(question.text());
+                    question.answers().forEach(System.out::println);
+                    System.out.println();
+                }
+        );
     }
 }
