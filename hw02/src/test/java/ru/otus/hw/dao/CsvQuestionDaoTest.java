1e4c5dccf00276f4e603d37f5833f0f82739a6e0
discribe <jaguar_85@mail.ru>
2024-08-21 22:44:48 +0300

    В UT замокано получение имени файла

--- hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
+++ hw02/src/test/java/ru/otus/hw/dao/CsvQuestionDaoTest.java
@@ -1,7 +1,9 @@
 package ru.otus.hw.dao;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import ru.otus.hw.config.AppProperties;
 import ru.otus.hw.config.TestFileNameProvider;
 import ru.otus.hw.exceptions.QuestionReadException;
@@ -11,10 +13,17 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class CsvQuestionDaoTest {
 
+    private TestFileNameProvider fileNameProvider;
+
+    @BeforeEach
+    void beforeAll(){
+        fileNameProvider = Mockito.mock(AppProperties.class);
+    }
+
     @Test
     @DisplayName("При отсутствии файла csv корректное отлавливание QuestionReadException")
     void fileCsvNotFound() {
-        TestFileNameProvider fileNameProvider = new AppProperties("questionsNotFound.xml");
+        Mockito.when(fileNameProvider.getTestFileName()).thenAnswer(invocationOnMock -> "sdf");
         assertThrows(QuestionReadException.class, () -> {
             QuestionDao questionDao = new CsvQuestionDao(fileNameProvider);
             questionDao.findAll();
@@ -24,7 +33,7 @@ class CsvQuestionDaoTest {
     @Test
     @DisplayName("Корректное считывание файла и преобразования в POJO обьекты")
     void fileCsvCorrectionRead() {
-        TestFileNameProvider fileNameProvider = new AppProperties("questionsSuccess.csv");
+        Mockito.when(fileNameProvider.getTestFileName()).thenAnswer(invocationOnMock -> "questionsSuccess.csv");
         QuestionDao questionDao = new CsvQuestionDao(fileNameProvider);
         var result = questionDao.findAll();
 
